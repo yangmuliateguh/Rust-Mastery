af77e5d5b2af19e5ee3770a0253c25d391b2d5f5
yangmuliateguh <teguh.kahfiyasin@gmail.com>
2026-02-02 23:34:57 +0700

    Update: 2026-02-02 23:34:57

---
+++ trait/multiple trait bounds dengan +.rs
@@ -0,0 +1,31 @@
+use std::fmt::Display;
+
+trait Log {
+    fn log(&self);
+}
+
+struct Event {
+    msg: String
+}
+
+impl Display for Event {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.msg)
+    }
+}
+
+impl Log for Event {
+    fn log(&self) {
+        println!("[LOG]: {}", self);
+    }
+}
+
+fn print_log<T: Display + Log>(item: T) {
+    println!("[System] {}", item);
+    item.log();
+}
+
+fn main(){
+    let e = Event {msg: "asikjuga uyy".into()};
+    print_log(e);
+}
